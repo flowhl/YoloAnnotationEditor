32302ee57cd7e7f88876b16257a99e0966a67e1c
Florian Wahl <florianwahl2@gmail.com>
2025-04-25 19:56:54 +0200

    Unit tests + update manager

---
+++ YoloAnnotationEditor/YoloAnnotationEditor/AppUpdateManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Velopack.Sources;
+using Velopack;
+using MessageBox = System.Windows.MessageBox;
+
+namespace YoloAnnotationEditor
+{
+    public static class AppUpdateManager
+    {
+        public static void CheckForUpdates()
+        {
+            var githubSource = new GithubSource("https://github.com/flowhl/YoloAnnotationEditor", null, false);
+
+            var mgr = new UpdateManager(githubSource, new UpdateOptions { AllowVersionDowngrade = false });
+            // check for new version
+            var newVersion = mgr.CheckForUpdates();
+            if (newVersion == null)
+                return; // no update available
+
+            //ask user if they want to update
+            var result = MessageBox.Show($"New version {newVersion?.TargetFullRelease?.Version.ToString()} available. Do you want to update?", "Update available", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.No)
+                return;
+
+            // download new version
+            mgr.DownloadUpdates(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
+    }
+}
--- YoloAnnotationEditor/YoloAnnotationEditor/MainWindow.xaml.cs
+++ YoloAnnotationEditor/YoloAnnotationEditor/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Velopack;
 
 namespace YoloAnnotationEditor;
 
@@ -18,6 +19,23 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        Loaded += MainWindow_Loaded;
+        VelopackApp.Build().Run();
         InitializeComponent();
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+#if DEBUG
+#else
+                AppUpdates.AppUpdateManager.CheckForUpdates();
+#endif
+        }
+        catch (Exception ex)
+        {
+            Notify.sendWarn("Error checking for updates: " + ex.Message);
+        }
     }
 }
